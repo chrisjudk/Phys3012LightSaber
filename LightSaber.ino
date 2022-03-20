a90b9848b3c1d378cb8940ce39c9f54e819c59a4
Chris Judkins <chrisjudk@protonmail.com>
2022-03-20 19:54:29 -0400

    Update with current code

--- LightSaber.ino
+++ LightSaber.ino
@@ -1,3 +1,4 @@
+//LightSaber V2.0
 #include <Adafruit_NeoPixel.h>
 
 //LED output on pin 6
@@ -5,178 +6,130 @@
 
 //define number of LEDs on strip
 #define LED_COUNT 120
-#define BRIGHTNESS 255
+#define BRIGHTNESS 255 //max 255
 
-bool bsA = false;
-bool bsB = false;
-bool bsC = false;
-bool bsAprev= false;
-bool bsBprev= false;
-bool isOn = false;
-int count = 0;
-int red = 0;
-int green = 0;
-int blue = 0;
-
-int t = 1;//delay time as we step through to get to the correct color
-
-//pins
-int iA = 2;//rotary encoder dataA pin
-int iB = 4;//rotary encoder dataB pin
-int iC = 3;//push button function
-
+//bool bsA = false;
+//bool bsB = false;
+//bool bsC = false;
+//bool bsAprev= false;
+//bool bsBprev= false;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+//################################################################
+//Variables
+  
+  //pins
+  //int iA = 2;//rotary encoder dataA pin
+  //int iB = 4;//rotary encoder dataB pin
+  int iC = 3;//toggle on function
+  int analogIn = A5;
+
+//configable
+  const int numColors = 11;
+  const int t = 5;//delay time as we step through to get to the correct color
+
+uint32_t myColor[numColors];
+uint32_t off = strip.Color(0,0,0);
+bool isOn = false;
+bool wasOn = true;
+int pot = 0;
+
 void setup()
 {
   Serial.begin(38400);
-  pinMode(iB, INPUT);
   pinMode(iC, INPUT);
-  attachInterrupt(0,rotated,CHANGE);
   strip.begin(); //INIT
   strip.show(); //Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS);
-  strip.setPixelColor(1,255,0,0);
+  strip.setPixelColor(strip.numPixels()-1,strip.gamma32(strip.ColorHSV(0,100,50)));
+  strip.setPixelColor(1,strip.gamma32(strip.ColorHSV(0,100,50)));
   strip.show();
-  Serial.println("Setup done");
-}
+  //array length is numColors. This variable stores our array length
+  myColor[0] = strip.gamma32(ColorHSLtoHSV(0,100,55));//Red
+  myColor[1] = strip.gamma32(ColorHSLtoHSV(24,100,100));//Orange
+  myColor[2] = strip.gamma32(ColorHSLtoHSV(42,100,98));//Yellow
+  myColor[3] = strip.gamma32(ColorHSLtoHSV(67,100,85));//lime
+  myColor[4] = strip.gamma32(ColorHSLtoHSV(120,100,100));//Green
+  myColor[5] = strip.gamma32(ColorHSLtoHSV(180,100,100));//cyan
+  myColor[6] = strip.gamma32(ColorHSLtoHSV(240,100,100));//Blue
+  myColor[7] = strip.gamma32(ColorHSLtoHSV(271,50,40));//purple
+  myColor[8] = strip.gamma32(ColorHSLtoHSV(300,100,100));//Magenta
+  myColor[9] = strip.gamma32(ColorHSLtoHSV(336,54,98));//Pinkish
+  myColor[10] = strip.gamma32(ColorHSLtoHSV(0,0,100));//white
+  delay(1000);
+  strip.setPixelColor(1,off);
+  strip.setPixelColor(118,off);
+  Serial.println("Setup done!");
+}//setup()
 
 void loop() 
 {
   isOn = digitalRead(iC);
-  int first = 0;
-  int last = 0;
   if(isOn)
   {
-    last = strip.numPixels()-1;
-    if((red + green + blue) == 0)
-      red = 255;
-  
-    for(int i = 0; i < strip.numPixels();i++)
+    pot = analogRead(analogIn);//read in potentiometer value //[0,1023]
+    int index = pot/(1023/numColors+1);
+    Serial.print("Index: ");
+    Serial.print(index);
+    Serial.print(", pot: ");
+    Serial.print(pot);
+    Serial.print(", rgb: ");
+    if (index < numColors)
     {
-      strip.setPixelColor(i,strip.Color(red,green,blue));
-      strip.show();
-      delay(t);
+      Serial.println(myColor[index]);
+      ColorPickyBoy(myColor[index]);
     }
-  }//if turn on
-  else if(!isOn)
+    else
+    {
+      Serial.print("Array index trying to go out of bounds! Index was: ");
+      Serial.println(index);
+    }//else
+    wasOn = true;
+  }//if on
+  if(!isOn && wasOn)
   {
-    for(int i = strip.numPixels()-1;i>=0;i--)
+    int i = strip.numPixels()/2 - 1;
+    int j = strip.numPixels()/2;
+    while(i >= 0)
     {
-      strip.setPixelColor(i,strip.Color(0,0,0));
+      strip.setPixelColor(i,off);
+      strip.setPixelColor(j,off);
       strip.show();
       delay(t);
+      Serial.print("i is: ");
+      Serial.print(i);
+      Serial.print(", j is: ");
+      Serial.println(j);
+      i--;
+      j++;
     }
+    wasOn = false;
   }//turn off
 }//loop()
 
-void colorPickyBoy()
-{
-  delay(20);
-  //red, orange(R,analog 128), yellow(RG), green, turquoise(BG), White(RGB)
-  
-  //select the color
-  if (count == 0)
-  {
-    setColor(0xFF,0x0,0x0);
-  }//if red
-  
-  else if (count == 1)
-  {
-    setColor(0xFF,0xA5,0x0);
-  }//if orange
-
-  else if (count == 2)
-  {
-     setColor(0xFF,0xFF,0x0);  
-  }//if yellow
-
-  else if (count == 3)
-  {
-    setColor(0x0,0xFF,0x0);
-  }//if green
-
-  else if (count == 4)
-  {
-    setColor(0x0,0xFF,0xFF);
-  }//if cyan
-
-  else if (count == 5)
-  {
-    setColor(0x0,0x0,0xFF);
-    Serial.println("It should be blue");
-  }//if blue
-
-  else if (count == 6)
-  {
-    setColor(0x93,0x70,0xDB);
-  }//if White
-}//colorPickyBoy()
-
-void setColor(int aRed, int aGreen, int aBlue)
-{
-  /*
-  while((red != aRed) || (green != aGreen) || (blue != aBlue))
-  {
-    while(red != aRed)
-    {
-      if (red > aRed)
-        red--;
-      else
-        red++;
-    }//red
-    
-    while(green != aGreen)
-    {
-      if (green > aGreen)
-        green--;
-      else
-        green++;
-    }//green
-    
-    while(blue != aBlue)
-    {
-      if (blue > aBlue)
-        blue--;
-      else
-        blue++;
-    }//blue
-    */
-    red = aRed;
-    green = aGreen;
-    blue = aBlue;
-    if(isOn)
-    {
-      for(int i=0; i < strip.numPixels(); i++)
-      {
-        strip.setPixelColor(i, strip.Color(red,green,blue));
-      }//for
-      delay(50);
-      strip.show();
-    }//if on
-}//setColor
-
-void rotated()
+void ColorPickyBoy(uint32_t color)
 {
   if (isOn)
   {
-    if(digitalRead(iA) == digitalRead(iB))
-      count++;
-    else
-      count--;
-    
-    if (count < 0)
+    int i = 0;
+    int j = strip.numPixels()-1;
+    while(i < 60)
     {
-      Serial.println("A");
-      count = 6;
-    }
-    if(count > 6)
-    {
-      count = 0;
-    }
-    Serial.print("Count is: ");
-    Serial.println(count);
-    delay(20);
-    colorPickyBoy();
-  }
-}//rotated()
+      strip.setPixelColor(i, color);
+      strip.setPixelColor(j,color);
+      delay(t);
+      strip.show();
+      i++;
+      j--;
+    }//while
+  }//if on
+}//colorPickyBoy()
+
+uint32_t ColorHSLtoHSV(int hue, int sat, int level)
+{
+  unsigned int tHue = (65536/360) * hue;
+  unsigned int tSat = (255/100) * sat;
+  unsigned int tValue = (255/100) * level;
+  return strip.ColorHSV(tHue, tSat, tValue);
+}
