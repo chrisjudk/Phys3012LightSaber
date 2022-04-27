b2219f0af9b04014bf808259448aff036e974d8a
Chris Judkins <chrisjudk@protonmail.com>
2022-04-27 10:35:22 -0400

    Update LightSaber.ino

--- LightSaber/LightSaber.ino
+++ LightSaber/LightSaber.ino
@@ -1,45 +1,34 @@
 //LightSaber V3.3
 #include <FastLED.h>
 
-
 //define number of LEDs on strip
 #define NUM_LEDS 120
-//#define BRIGHTNESS 100 //max 255
-
-//bool bsA = false;
-//bool bsB = false;
-//bool bsC = false;
-//bool bsAprev= false;
-//bool bsBprev= false;
-
-//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-
 
 /*
 ################################################################################
 configable Variables
 */
+
 //pins
   #define LED_PIN 6
-  //int iA = 2;//rotary encoder dataA
-  //int iB = 4;//rotary encoder dataB
+  #define PBU 2
+  #define PBD 3
   #define TOGGLE 3//toggle on function
   #define ANALOG_IN A5//input from potentiometer
-
 
 #define MY_BRIGHTNESS 180//brightness 0-255
 #define NUM_COLORS 11
 const int t = 5;//delay time as we step through to get to the correct color
 
-
-
 /*
 ################################################################################
 Variables
 */
+
 CRGB leds[NUM_LEDS];
-uint16_t pot = 0;
+volatile uint8_t count = 0;
+ulong prevIntUp = 0;
+ulong prevIntDown = 0;
 bool isOn = false;
 bool wasOn = true;
 CHSV myColor[NUM_COLORS];
@@ -49,15 +38,22 @@ CRGB off = CRGB(0,0,0);
 ################################################################################
 Setup
 */
+
 void setup()
 {
   Serial.begin(38400);
   pinMode(TOGGLE, INPUT);
+  pinMode(PBU, INPUT);
+  pinMode(PBD, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(PBU), NextColorBoy, RISING);
+  attachInterrupt(digitalPinToInterrupt(PBD), PrevColorBoy, RISING);
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);//setup the led strip
   FastLED.setBrightness(MY_BRIGHTNESS);//set brightness 0-255
   FastLED.setMaxPowerInMilliWatts(25000);
   FastLED.setCorrection(TypicalPixelString);//Might need this to get proper colors
+
   FastLED.showColor(off);//set all LEDs to black immediately
   leds[0]   = CRGB(255,0,0);//set 1st led red
   leds[1]   = CRGB(0,255,0);//second green
@@ -66,6 +62,7 @@ void setup()
   leds[118] = CRGB(0,255,0);//2nd to last green
   leds[119] = CRGB(255,0,0);//set last led red
   FastLED.show();
+
   //array length is NUM_COLORS. This variable stores our array length
   myColor[0]  = CHSV(0,255,142);//Red
   myColor[1]  = CHSV(24,255,255);//Orange
@@ -78,42 +75,37 @@ void setup()
   myColor[8]  = CHSV(210,255,255);//Magenta
   myColor[9]  = CHSV(220,140,250);//Pinkish
   myColor[10] = CHSV(0,0,255);//white
+
   FastLED.delay(1000);
   FastLED.showColor(off);
   FastLED.show();
   Serial.println("Setup done!");
 }//setup()
 
-
-
 /*
 ################################################################################
 loop()
 */
+
 void loop()
 {
   isOn = digitalRead(TOGGLE);
   if(isOn)
   {
     pot = analogRead(ANALOG_IN);//read in potentiometer value //[0,1023]
-    uint8_t index = map(pot, 0, 1023, 0, NUM_COLORS - 1);
-    // Serial.print("Index: ");
-    // Serial.print(index);
-    // Serial.print(", pot: ");
-    // Serial.print(pot);
-    // Serial.print(", rgb: ");
-    Serial.print(index);
+
+    Serial.print(count);
     Serial.print("/");
     Serial.println(NUM_COLORS - 1);
-    if (index < NUM_COLORS)
+    if (count < NUM_COLORS)
     {
       // Serial.println(myColor[index]);
-      ColorPickyBoy(myColor[index]);
+      ColorPickyBoy(myColor[count]);
     }
     else
     {
       Serial.print("Array index trying to go out of bounds! Index was: ");
-      Serial.println(index);
+      Serial.println(count);
     }//else
     wasOn = true;
   }//if on
@@ -139,12 +131,11 @@ void loop()
   }//turn off
 }//loop()
 
-
-
 /*
 ################################################################################
 Methods
 */
+
 void ColorPickyBoy(CHSV color)
 {
   if (isOn)
@@ -167,6 +158,57 @@ void ColorPickyBoy(CHSV color)
     Serial.println("\n--------------------------------------------------");
   }//if on
 }//colorPickyBoy()
+
+void NextColorBoy()
+{
+  ulong interruptTime = millis();
+  if (interruptTime - prevIntUp > 50)
+  {
+    if (count < 10)
+      count++;
+    else
+      count = 0;
+  }
+  prevIntUp = interruptTime;
+}
+
+void PrevColorBoy()
+{
+  ulong interruptTime = millis()
+  if (interruptTime - prevIntDown > 50)
+  {
+    if (count > 0)
+      count--;
+    else
+      count = 10;
+  }
+  prevIntDown = interruptTime;
+}
+
+/*
+Anything down here is not actually in use, will be removed for production version
+*/
+
+//#define BRIGHTNESS 100 //max 255
+
+//int iA = 2;//rotary encoder dataA
+//int iB = 4;//rotary encoder dataB
+
+// Serial.print("Index: ");
+// Serial.print(index);
+// Serial.print(", pot: ");
+// Serial.print(pot);
+// Serial.print(", rgb: ");
+
+//bool bsA = false;
+//bool bsB = false;
+//bool bsC = false;
+//bool bsAprev= false;
+//bool bsBprev= false;
+
+//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
 /*
 CHSV ColorHSLtoHSV(uint16_t hue, uint8_t sat, uint8_t level)
 {
